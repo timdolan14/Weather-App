1a42108d706e5f8b9da26176c0a96adbc46a2d61
Tim Dolan <timmydolan14@gmail.com>
2023-05-23 16:34:03 -0400

    Five Day Working

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ var APIkey = "72de6b11fa4df733f08169c8a7643196"
 
 // Fetch Request
 
-var cityAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=' + APIkey
-fetch (cityAPI) 
-    .then (function (response) {
-        console.log(response);
-        return response.json ();
-        }).then (function (data) {
-            console.log(data);
-        })
+var cityAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${"New York"}&appid=${APIkey}&units=imperial`
+fetch(cityAPI)
+    .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        for (let i = 0; i < data.list.length; i+=8) {
+            var day = data.list[i]
+            fiveDay.innerHTML += `<div class="forecast-day"><h3>${day.dt_txt}</h3> <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"/><p>Temp:${day.main.temp}</p>    <p>Wind:${day.wind.speed}</p><p>Humidity:${day.main.humidity}</p></div>`
+        }
+    })
